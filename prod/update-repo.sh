3eaca831cb260f5014c9ed0f4d93f689c142614e
RDMX87 <83808648+RDMX87@users.noreply.github.com>
2021-06-15 10:21:36 +0300

    Update update-repo.sh

--- prod/update-repo.sh
+++ prod/update-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat pkgs.txt | while read pkg; do
+cat prod/pkgs.txt | while read pkg; do
   if [[ ! -z $pkg ]]; then
     check_update_str=$(yum check-update $pkg | grep $pkg)
     if [[ ! -z $check_update_str ]]; then
@@ -8,13 +8,13 @@ cat pkgs.txt | while read pkg; do
       pkg_arch=$(echo $check_update_str | awk '{ print $1 }' | cut -d. -f2)
       pkg_version=$(echo $check_update_str | awk '{ print $2 }')
       pkg_file_name_version=$(echo $pkg_version | cut -d: -f2)
-      if [[ ! -e "7/os/x86_64/Packages/$pkg_file-$pkg_file_name_version.$pkg_arch.rpm" ]]; then
-        yum update --downloadonly --downloaddir=7/os/x86_64/Packages $pkg_file-$pkg_version.$pkg_arch
+      if [[ ! -e "prod/7/os/x86_64/Packages/$pkg_file-$pkg_file_name_version.$pkg_arch.rpm" ]]; then
+        yum update --downloadonly --downloaddir=prod/7/os/x86_64/Packages $pkg_file-$pkg_version.$pkg_arch
       fi
     fi
   fi
 done
 
-createrepo --update 7/os/x86_64
+createrepo --update prod/7/os/x86_64
 
-chown -R user:user 7/os/x86_64
+chown -R user:user prod/7/os/x86_64
