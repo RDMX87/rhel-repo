ec892fd2b1a9794ca05b96706616962dfd6bb274
RDMX87 <83808648+RDMX87@users.noreply.github.com>
2021-06-15 11:28:25 +0300

    Update update-repo.sh

--- prod/update-repo.sh
+++ prod/update-repo.sh
@@ -2,14 +2,14 @@
 
 cat prod/pkgs.txt | while read pkg; do
   if [[ ! -z $pkg ]]; then
-    check_update_str=$(yum check-update $pkg | grep $pkg)
+    check_update_str=$(yum list $pkg | tail -1)
     if [[ ! -z $check_update_str ]]; then
       pkg_file=$(echo $check_update_str | awk '{ print $1 }' | cut -d. -f1)
       pkg_arch=$(echo $check_update_str | awk '{ print $1 }' | cut -d. -f2)
       pkg_version=$(echo $check_update_str | awk '{ print $2 }')
       pkg_file_name_version=$(echo $pkg_version | cut -d: -f2)
       if [[ ! -e "prod/7/os/x86_64/Packages/$pkg_file-$pkg_file_name_version.$pkg_arch.rpm" ]]; then
-        yumdownloader --destdir=prod/7/os/x86_64/Packages --resolve $pkg_file-$pkg_version.$pkg_arch
+        yumdownloader --destdir=prod/7/os/x86_64/Packages $pkg_file-$pkg_version.$pkg_arch
       fi
     fi
   fi
