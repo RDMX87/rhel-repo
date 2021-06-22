cbcff45b2fb36995c4d22f338595a10abf904fc7
RDMX87 <83808648+RDMX87@users.noreply.github.com>
2021-06-22 15:51:55 +0300

    Update update-repo.sh

--- update-repo.sh
+++ update-repo.sh
@@ -6,6 +6,9 @@ cat pkgs.txt | while read pkg; do
     if [[ ! -z $check_update_str ]]; then
       pkg_file=$(echo $check_update_str | awk '{ print $1 }' | cut -d. -f1)
       pkg_arch=$(echo $check_update_str | awk '{ print $1 }' | cut -d. -f2)
+      if [[ $pkg_arch == "i686" ]]; then
+        pkg_arch="x86_64"
+      fi
       pkg_version=$(echo $check_update_str | awk '{ print $2 }')
       pkg_file_name_version=$(echo $pkg_version | cut -d: -f2)
       if [[ ! -e "7/os/x86_64/Packages/$pkg_file-$pkg_file_name_version.$pkg_arch.rpm" ]]; then
